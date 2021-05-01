ee2e75cc390ceda834d1a9a6338b4e188401eed9
laverpearl <k2811jj@gmail.com>
2021-05-02 02:15:47 +0900

    파일 정리

--- Step2/2884.py
+++ Step2/2884.py
@@ -1,9 +1,11 @@
+# 두 수를 a b 이렇게 받는다면 
 h,m = map(int,input().split())
 
 if m < 45 and h > 0:
     h = h - 1
     m = m + 15
     print(h, m)
+# 0시일때를 생각해야한다.
 elif m < 45 and h == 0:
     h = 23
     m = m + 15
