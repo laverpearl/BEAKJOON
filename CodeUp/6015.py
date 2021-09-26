e07f0fd0a776f2162521c769745a22c1b1dd270b
laverpearl <k2811jj@gamil.com>
2021-09-27 01:41:43 +0900

    코드업 100

---
+++ CodeUp/6015.py
@@ -0,0 +1,16 @@
+"""
+
+
+공백을 두고 입력된 정수 2개를 입력받아 줄을 바꿔 출력
+
+python의 input()은 한 줄 단위로 입력을 받는다
+
+a, b = input().split()을 사용하면 공백을 기준으로 값들을 나누어(split) 자른다
+
+
+"""
+
+
+a, b = input().split()
+print(a)
+print(b)
