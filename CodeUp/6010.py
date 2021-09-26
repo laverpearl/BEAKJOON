cfcfc60e11d5e1d93e77d7a07c39f9622464d63b
laverpearl <k2811jj@gamil.com>
2021-09-27 01:37:48 +0900

    코드업100

---
+++ CodeUp/6010.py
@@ -0,0 +1,16 @@
+"""
+
+
+변수에 정수값을 저장한 후 정수로 변환하여 출력
+
+예시
+
+n = input()
+n = int(n)
+print(n)
+
+
+"""
+
+n = int(input())
+print(n)
---
+++ CodeUp/6014.py
@@ -0,0 +1,7 @@
+# 입력받은 실수를 줄을 바꿔 3번 출력한다
+
+
+a = input()
+print(a)
+print(a)
+print(a)
